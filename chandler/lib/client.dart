337257a57502465b6d64c0f5c16ddb8527bc1c17
Oleg E. Vorobiov <isnullxbh@gmail.com>
2024-01-16 17:09:13 +0700

    Add functions to send commands to MCU

--- chandler/lib/client.dart
+++ chandler/lib/client.dart
@@ -1,26 +1,24 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:chandler/mcu/led_strip_state.dart';
+import 'package:chandler/mcu/light_sensor_dependency.dart';
+
 class Client {
   final RawDatagramSocket _socket;
   final InternetAddress   _address;
   final int               _port;
 
-  void _handleData(Uint8List data) {
-    print("Data received: $data");
-  }
-
-  void _handleError(error) {
-    print("An error occurred while reading: $error");
-  }
+  void Function(Uint8List data)? onMessageReceived;
 
   Client._construct(this._socket, this._address, this._port) {
     _socket.listen((event) {
       switch (event) {
         case RawSocketEvent.read:
-          print("Receiving datagram...");
           final datagram = _socket.receive();
-          print("Receiving datagram...Done");
+          if (datagram != null && onMessageReceived != null) {
+            onMessageReceived!(datagram.data);
+          }
           break;
 
         case RawSocketEvent.closed:
@@ -36,6 +34,51 @@ class Client {
   static Future<Client> connect(InternetAddress host, int port) async {
     final socket = RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
     return socket.then((socket) => Client._construct(socket, host, port));
+  }
+
+  void enableRelay(int relayIndex) {
+    print("Send command to enable relay ${relayIndex}...");
+    var data = Uint8List(2);
+    data[0] = relayIndex + 1;
+    data[1] = 1;
+    _socket.send(data, _address, _port);
+    print("Send command to enable relay ${relayIndex}...Done");
+  }
+
+  void disableRelay(int relayIndex) {
+    print("Send command to disable relay ${relayIndex}...");
+    var data = Uint8List(2);
+    data[0] = relayIndex + 1;
+    data[1] = 0;
+    _socket.send(data, _address, _port);
+    print("Send command to disable relay ${relayIndex}...Done");
+  }
+
+  void setLightSensorDependencyForRelay(int relayIndex, LightSensorDependency mode) {
+    print("Send command to set light sensor dependency mode for relay $relayIndex...");
+    var data = Uint8List(2);
+    data[0] = relayIndex + 3;
+    data[1] = mode.index;
+    _socket.send(data, _address, _port);
+    print("Send command to set light sensor dependency mode for relay $relayIndex...Done");
+  }
+
+  void setLedStripState(LedStripState state) {
+    print("Send command to set LED strip state...");
+    var data = Uint8List(2);
+    data[0] = 0x06;
+    data[1] = state.index;
+    _socket.send(data, _address, _port);
+    print("Send command to set LED strip state...");
+  }
+
+  void setLightSensorDependencyForLedStrip(LightSensorDependency mode) {
+    print("Send command to set light sensor dependency mode for LED strip...");
+    var data = Uint8List(2);
+    data[0] = 0x07;
+    data[1] = mode.index;
+    _socket.send(data, _address, _port);
+    print("Send command to set light sensor dependency mode for LED strip...Done");
   }
 
   Future<bool> getState() async {
