67ae7976c537d3d31fc7b19d7f2beb7dfb3f57e9
Oleg E. Vorobiov <isnullxbh@gmail.com>
2024-01-18 14:25:00 +0700

    Get MCU state to show proper values on periphery control page

--- chandler/lib/pages/home.dart
+++ chandler/lib/pages/home.dart
@@ -1,29 +1,21 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:chandler/mcu/state.dart';
 import 'package:chandler/pages/periphery_management.dart';
 import 'package:flutter/material.dart';
 import 'package:chandler/client.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
+
+  const MyHomePage({super.key, required this.title});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int    _counter = 0;
   String _status = "";
   final  _endpointController = TextEditingController();
   BuildContext? _context;
@@ -51,23 +43,22 @@ class _MyHomePageState extends State<MyHomePage> {
     final port = int.tryParse(endpoint.substring(separator + 1));
 
     if (host == null || port == null) {
-      updateStatus("Invalid host or(and) port");
+      updateStatus("Invalid host/port");
       return;
     }
 
-    updateStatus("Connecting to $endpoint...");
-
-    final client = Client.connect(host, port);
-
-    client.then(onConnectionEstablished)
-          .catchError(handleConnectionError);
-
-    updateStatus("Connecting to $endpoint... Done");
+    Client.connect(host, port)
+        .then(onConnectionEstablished)
+        .catchError(handleConnectionError);
   }
 
   void onConnectionEstablished(Client client) {
-    Navigator.push(_context!,
-        MaterialPageRoute(builder: (context) => PeripheryManagementPage(client: client)));
+    client.onMessageReceived = (Uint8List data) {
+      final state = McuState.createFrom(data);
+      final route = MaterialPageRoute(builder: (context) => PeripheryManagementPage(client: client, mcuState: state));
+      Navigator.push(_context!, route);
+    };
+    client.getState().ignore();
   }
 
   void handleConnectionError(error) {
@@ -77,39 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     _context = context;
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        // backgroundColor: Colors.black54,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(_status),
